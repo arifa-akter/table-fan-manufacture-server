516b0253b0c48f588734304047a9c0c013277fa7
arifa-akter <arifaakterchoity@gmail.com>
2022-05-23 23:14:29 +0600

    add review and user

--- index.js
+++ index.js
@@ -12,11 +12,65 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.czo1g.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+ 
 async function run(){
     try{
         await client.connect();
-        console.log('data base connected')
+     // tableFanToolsCollection
+     const toolsCollection = client.db('table_fan').collection('tools')
+    //  table user collection
+    const userCollection = client.db('table_fan').collection('users')
+    //  table user collection
+    const reviewCollection = client.db('table_fan').collection('review')
+
+    // get user fan tool
+     app.get('/tools' ,async(req,res)=>{
+      const query ={}
+      const cursor = toolsCollection.find(query)
+      const tools = await cursor.toArray()
+      res.send(tools)
+     })
+
+     // tools add product
+     app.post('/tools' ,async(req,res)=>{
+      const tools = req.body
+      const result = await toolsCollection .insertOne(tools)
+      res.send(result)
+    })
+     // post review part start
+     app.post('/review' ,async(req,res)=>{
+      const tools = req.body
+      const result = await reviewCollection .insertOne(tools)
+      res.send(result)
+    })
+    // get review
+      app.get('/review' ,async(req,res)=>{
+      const query ={}
+      const cursor = reviewCollection.find(query)
+      const review = await cursor.toArray()
+      res.send(review)
+     })
+    
+      // user update in mongodb  
+      app.put('/user/:email' ,async(req,res)=>{
+        const email=req.params.email
+        const user = req.body
+        const filter = { email:email };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set:user
+        };
+        const result = await userCollection.updateOne(filter, updateDoc, options);
+      })
+
+      // get all user
+      app.get('/user' ,async(req,res)=>{
+        const query ={}
+        const cursor = userCollection.find(query)
+        const tools = await cursor.toArray()
+        res.send(tools)
+       })
+
     }
     finally{
 
@@ -24,12 +78,6 @@ async function run(){
 }
 run().catch(console.dir);
 
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   console.log('db connect')
-//   // perform actions on the collection object
-//   client.close();
-// });
 
 app.get('/', (req, res) => {
   res.send('Hello World table fan parts!')
