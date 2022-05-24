7f33db239f34e8d85a8c22373149196e370bfa8f
arifa-akter <arifaakterchoity@gmail.com>
2022-05-24 12:45:38 +0600

    add admin verification

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const jwt = require ('jsonwebtoken');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion ,ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
@@ -12,7 +13,21 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.czo1g.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
- 
+ // jwt verify function
+function verifyJwt(req, res ,next){
+  const authHeader =req.headers.authorization 
+  if(!authHeader){
+    return res.status(401).send({message:'UnAuthorized access'})
+  }
+  const token = authHeader.split(' ')[1]
+  jwt.verify(token, process.env.DB_TOKEN, function(err, decoded) {
+    if(err){
+     return res.status(403).send({message:'Forbidden access'})
+    }
+    req.decoded = decoded
+    next()
+  });
+}
 async function run(){
     try{
         await client.connect();
@@ -22,7 +37,18 @@ async function run(){
     const userCollection = client.db('table_fan').collection('users')
     //  table user collection
     const reviewCollection = client.db('table_fan').collection('review')
+    //  admin verify
+     const verifyAdmin = async (req,res ,next) =>{
+      const requester = req.decoded.email 
+      const requestedAccount = await userCollection.findOne({email:requester})
+      if(requestedAccount.role === 'admin'){
+        next()
+      }
+      else{
+        res.status(403).send({message:'forbidden'})
+      }
 
+    }
     // get user fan tool
      app.get('/tools' ,async(req,res)=>{
       const query ={}
@@ -68,15 +94,36 @@ async function run(){
           $set:user
         };
         const result = await userCollection.updateOne(filter, updateDoc, options);
+        const token = jwt.sign({email:email }, process.env.DB_TOKEN , {  expiresIn: '24h' });
+        res.send({result ,token })
       })
 
       // get all user
-      app.get('/user' ,async(req,res)=>{
+      app.get('/user' ,verifyJwt ,async(req,res)=>{
         const query ={}
         const cursor = userCollection.find(query)
         const tools = await cursor.toArray()
         res.send(tools)
        })
+
+      // creat admin 
+       app.put('/user/admin/:email' ,verifyJwt,verifyAdmin,async(req,res)=>{
+        const email=req.params.email
+        const filter = { email:email };
+        const updateDoc = {
+          $set:{role:'admin'}
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result)  
+      })
+       // get admin form user
+       app.get('/admin/:email' , async(req , res)=>{
+        const email =req.params.email 
+        const user =await userCollection.findOne({email:email})
+        const isAdmin = user.role === 'admin'
+        res.send({admin:isAdmin})
+
+      })
 
     }
     finally{
