2e67a8f8e71b1877c43ebeff1b1d8e406a8fa3c2
arifa-akter <arifaakterchoity@gmail.com>
2022-05-26 09:26:28 +0600

    add cors file

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const { MongoClient, ServerApiVersion ,ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 // middle
-app.use(cors())
+app.use(cors());
+const corsConfig = {
+    origin: true,
+    credentials: true,
+}
+app.use(cors(corsConfig))
+app.options('*', cors(corsConfig))
 app.use(express.json())
 
 // mongodb connection
